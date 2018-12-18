ed8a57816571d435b3c2fbad1478052f4a2e27d1
Filippo Orazi <filippo.orazi@studio.unibo.it>
2018-12-18 13:13:15 +0100

    controllo duplicazione persona

--- codice (trascina questa cartella su Atom)/php/gestioneDB.php
+++ codice (trascina questa cartella su Atom)/php/gestioneDB.php
@@ -9,23 +9,29 @@ $psswduser = "";
 if(isset($_POST["nome"]) and isset($_POST["cognome"]) and isset($_POST["email"]) and isset($_POST["password"])){
   //preparazione query
   $psswduser = PASSWORD_HASH($_POST["password"], PASSWORD_DEFAULT);
-  $query_sql="INSERT INTO `clienti` (`nome`, `cognome`, `email`, `password`) VALUES ('".$_POST['nome']."', '"
+  $query_sql_insert="INSERT INTO `clienti` (`nome`, `cognome`, `email`, `password`) VALUES ('".$_POST['nome']."', '"
                                                                                .$_POST['cognome']."', '"
                                                                                .$_POST['email']."', '"
                                                                                .$psswduser."')";
+
+  $query_sql_retrive= " SELECT `email` FROM `clienti` where `email` = '".$_POST['email']."'";
   //connessione al db
   $conn =new mysqli($servername, $username, $password, $database);
   //Check della connessione
   if ($conn->connect_errno) {
       echo "Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
   }
+  if(empty($conn->query($query_sql_retrive))){
   //Invio query
-  if ($conn->query($query_sql) === TRUE) {
-      echo "Benvenuto " .$_POST['nome']. ", la registrazione è avvenuta con successo!";
-  } else {
-      echo "Errore: " . $query_sql . "<br>" . $conn->error;
+    if ($conn->query($query_sql) === TRUE) {
+        echo "Benvenuto " .$_POST['nome']. ", la registrazione è avvenuta con successo!";
+    } else {
+        echo "Errore: " . $query_sql . "<br>" . $conn->error;
+    }
+  }else{
+    echo "<script>console.log( 'you are already registered' );</script>";
   }
-  //Chiusura connessione con db
+    //Chiusura connessione con db
   $conn->close();
 }
  ?>
