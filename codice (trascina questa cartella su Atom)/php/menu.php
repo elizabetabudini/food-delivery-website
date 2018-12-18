d635a4619f254240571c95151c3a37beca6755d1
elizabetabudini <elizabeta.budini@studio.unibo.it>
2018-12-18 12:31:18 +0100

    push per filo

--- codice (trascina questa cartella su Atom)/php/menu.php
+++ codice (trascina questa cartella su Atom)/php/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
@@ -9,6 +9,7 @@
   <li class="nav-item active">
     <a class="nav-link" href="homeclienti.html">Home <span class="sr-only">(current)</span></a>
   </li>
+
   <li class="nav-item">
     <a class="nav-link" href="accedi.html">Accedi</a>
   </li>
