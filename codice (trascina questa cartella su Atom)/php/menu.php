e4110e5a08f54f862ee8e5a9b1779b906ca0812e
elizabetabudini <elizabeta.budini@studio.unibo.it>
2018-12-18 17:21:37 +0100

    risolto collegamenti pagine

--- codice (trascina questa cartella su Atom)/php/menu.php
+++ codice (trascina questa cartella su Atom)/php/menu.php
@@ -2,19 +2,19 @@
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
-<a class="navbar-brand" href="homeclienti.html">CFU - Cesena Food University</a>
+<a class="navbar-brand" href="homeclienti.php">CFU - Cesena Food University</a>
 
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
 <ul class="navbar-nav mr-auto">
   <li class="nav-item active">
-    <a class="nav-link" href="homeclienti.html">Home <span class="sr-only">(current)</span></a>
+    <a class="nav-link" href="homeclienti.php">Home <span class="sr-only">(current)</span></a>
   </li>
 
   <li class="nav-item">
-    <a class="nav-link" href="accedi.html">Accedi</a>
+    <a class="nav-link" href="accedi.php">Accedi</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="signinutente.html">Registrati</a>
+    <a class="nav-link" href="signinutente.php">Registrati</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Carrello</a>
