61990940bac33a1d6677366e57d93913448cbe1b
Filippo Orazi <filippo.orazi@studio.unibo.it>
2018-12-26 15:28:36 +0100

    lol sono un luogo

---
+++ database/cfu.sql
@@ -0,0 +1,318 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Dic 26, 2018 alle 15:27
+-- Versione del server: 10.1.37-MariaDB
+-- Versione PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cfu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `alimento`
+--
+
+CREATE TABLE `alimento` (
+  `disponibilita` char(1) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `info` varchar(100) NOT NULL,
+  `prezzo` decimal(5,2) NOT NULL,
+  `id_ristorante` int(11) NOT NULL,
+  `nome_menu` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `categoria_menu`
+--
+
+CREATE TABLE `categoria_menu` (
+  `nome_categoria` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `categoria_ristoranti`
+--
+
+CREATE TABLE `categoria_ristoranti` (
+  `nome_categoria` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `lista`
+--
+
+CREATE TABLE `lista` (
+  `id_prenotazione` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `id_ristorante` int(11) NOT NULL,
+  `nome_menu` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `luogo`
+--
+
+CREATE TABLE `luogo` (
+  `nome` char(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `luogo`
+--
+
+INSERT INTO `luogo` (`nome`) VALUES
+('aula 2.1'),
+('aula 2.4'),
+('aula 2.9'),
+('aula 3.7'),
+('aula 3.9'),
+('laboratorio 2.2'),
+('laboratorio 3.1'),
+('laboratorio 3.3'),
+('punto ristoro');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_ristorante` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `nome_categoria` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `notifica`
+--
+
+CREATE TABLE `notifica` (
+  `codice` int(11) NOT NULL,
+  `testo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `persona`
+--
+
+CREATE TABLE `persona` (
+  `nome` varchar(20) NOT NULL,
+  `cognome` varchar(20) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `id_ristorante` int(11) DEFAULT NULL,
+  `password` varchar(40) NOT NULL,
+  `privilegi` int(11) NOT NULL,
+  `cellulare` char(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `prenotazione`
+--
+
+CREATE TABLE `prenotazione` (
+  `info_prenotazione` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL,
+  `id_ristorante` int(11) NOT NULL,
+  `email_cliente` varchar(40) NOT NULL,
+  `data` date NOT NULL,
+  `ora_accettazione` date NOT NULL,
+  `stato` int(11) NOT NULL,
+  `ora_consegna` date NOT NULL,
+  `totale` int(11) NOT NULL,
+  `luogo_consegna` char(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `riceve`
+--
+
+CREATE TABLE `riceve` (
+  `codice_notifica` int(11) NOT NULL,
+  `email` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `ristorante`
+--
+
+CREATE TABLE `ristorante` (
+  `info` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL,
+  `email_proprietario` varchar(40) NOT NULL,
+  `nome_categoria` varchar(20) DEFAULT NULL,
+  `nome` varchar(20) NOT NULL,
+  `indirizzo` varchar(40) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `alimento`
+--
+ALTER TABLE `alimento`
+  ADD PRIMARY KEY (`nome`,`id_ristorante`,`nome_menu`),
+  ADD KEY `FKpartecipa` (`id_ristorante`,`nome_menu`);
+
+--
+-- Indici per le tabelle `categoria_menu`
+--
+ALTER TABLE `categoria_menu`
+  ADD PRIMARY KEY (`nome_categoria`);
+
+--
+-- Indici per le tabelle `categoria_ristoranti`
+--
+ALTER TABLE `categoria_ristoranti`
+  ADD PRIMARY KEY (`nome_categoria`);
+
+--
+-- Indici per le tabelle `lista`
+--
+ALTER TABLE `lista`
+  ADD PRIMARY KEY (`id_prenotazione`,`nome`,`id_ristorante`,`nome_menu`),
+  ADD KEY `FKcomprende` (`nome`,`id_ristorante`,`nome_menu`);
+
+--
+-- Indici per le tabelle `luogo`
+--
+ALTER TABLE `luogo`
+  ADD PRIMARY KEY (`nome`);
+
+--
+-- Indici per le tabelle `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_ristorante`,`nome`),
+  ADD KEY `FKpartecipa2` (`nome_categoria`);
+
+--
+-- Indici per le tabelle `notifica`
+--
+ALTER TABLE `notifica`
+  ADD PRIMARY KEY (`codice`);
+
+--
+-- Indici per le tabelle `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`email`),
+  ADD KEY `FKappartiene` (`id_ristorante`);
+
+--
+-- Indici per le tabelle `prenotazione`
+--
+ALTER TABLE `prenotazione`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKeffettua` (`email_cliente`),
+  ADD KEY `FKriferisce` (`id_ristorante`),
+  ADD KEY `FKconsegna` (`luogo_consegna`);
+
+--
+-- Indici per le tabelle `riceve`
+--
+ALTER TABLE `riceve`
+  ADD PRIMARY KEY (`email`,`codice_notifica`),
+  ADD KEY `FKric_not` (`codice_notifica`);
+
+--
+-- Indici per le tabelle `ristorante`
+--
+ALTER TABLE `ristorante`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `FKappartiene_ID` (`email_proprietario`),
+  ADD UNIQUE KEY `FKinclude_ID` (`nome_categoria`);
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `alimento`
+--
+ALTER TABLE `alimento`
+  ADD CONSTRAINT `FKpartecipa` FOREIGN KEY (`id_ristorante`,`nome_menu`) REFERENCES `menu` (`id_ristorante`, `nome`);
+
+--
+-- Limiti per la tabella `lista`
+--
+ALTER TABLE `lista`
+  ADD CONSTRAINT `FKcomprende` FOREIGN KEY (`nome`,`id_ristorante`,`nome_menu`) REFERENCES `alimento` (`nome`, `id_ristorante`, `nome_menu`),
+  ADD CONSTRAINT `FKlis_pre` FOREIGN KEY (`id_prenotazione`) REFERENCES `prenotazione` (`id`);
+
+--
+-- Limiti per la tabella `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `FKoffre` FOREIGN KEY (`id_ristorante`) REFERENCES `ristorante` (`id`),
+  ADD CONSTRAINT `FKpartecipa2` FOREIGN KEY (`nome_categoria`) REFERENCES `categoria_menu` (`nome_categoria`);
+
+--
+-- Limiti per la tabella `persona`
+--
+ALTER TABLE `persona`
+  ADD CONSTRAINT `FKappartiene` FOREIGN KEY (`id_ristorante`) REFERENCES `ristorante` (`id`);
+
+--
+-- Limiti per la tabella `prenotazione`
+--
+ALTER TABLE `prenotazione`
+  ADD CONSTRAINT `FKconsegna` FOREIGN KEY (`luogo_consegna`) REFERENCES `luogo` (`nome`),
+  ADD CONSTRAINT `FKeffettua` FOREIGN KEY (`email_cliente`) REFERENCES `persona` (`email`),
+  ADD CONSTRAINT `FKriferisce` FOREIGN KEY (`id_ristorante`) REFERENCES `ristorante` (`id`);
+
+--
+-- Limiti per la tabella `riceve`
+--
+ALTER TABLE `riceve`
+  ADD CONSTRAINT `FKric_not` FOREIGN KEY (`codice_notifica`) REFERENCES `notifica` (`codice`),
+  ADD CONSTRAINT `FKric_per` FOREIGN KEY (`email`) REFERENCES `persona` (`email`);
+
+--
+-- Limiti per la tabella `ristorante`
+--
+ALTER TABLE `ristorante`
+  ADD CONSTRAINT `FKappartiene_FK` FOREIGN KEY (`email_proprietario`) REFERENCES `persona` (`email`),
+  ADD CONSTRAINT `FKinclude_FK` FOREIGN KEY (`nome_categoria`) REFERENCES `categoria_ristoranti` (`nome_categoria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
