164848c3225d1cdd55b24947b5f28d8d0a6d100a
Filippo Orazi <filippo.orazi@studio.unibo.it>
2019-01-03 12:37:28 +0100

    ricerca window

--- codice/php/carrello.php
+++ codice/php/carrello.php
@@ -49,27 +49,32 @@ if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
 
-
-    foreach($conn->query('SELECT nome , id_ristorante, nome_menu FROM carrello WHERE id_prenotazione = '.$_SESSION['id'].'') as $row) {
-      //TODO
-      echo'
-        <div class="row card-sm">
-          <div class="card card-sm center-msg-box transparent col-sm-9">
-            <h3>'.$row["nome"].'</h3>
-            <h4>quantita</h4>
+    if (isset($_SESSION['id_prenotazione'])){
+        foreach($conn->query('SELECT nome , id_ristorante, nome_menu FROM carrello WHERE id_prenotazione = '.$_SESSION['id'].'') as $row) {
+        //TODO
+        echo'
+          <div class="row card-sm">
+            <div class="card card-sm center-msg-box transparent col-sm-9">
+              <h3>'.$row["nome"].'</h3>
+              <h4>quantita</h4>
+            </div>
+            <form action="#" method="post" id="form1" class = "card card-sm transparent col-sm-3">
+              <input type="submit" class="btn btn-primary" name="action" value="Modifica"/>
+              <br/>
+              <input type="submit" class="btn btn-primary" name="action" value="Cancella"/>
+              <input type="hidden" name = "id" value="'.$_SESSION["id_prenotazione"].'">
+              <input type="hidden" name = "nome_menu" value="'.$row["nome_menu"].'">
+              <input type="hidden" name = "id_ristorante" value="'.$row["id_ristorante"].'">
+              <input type="hidden" name = "nome" value="'.$row["nome"].'">
+              <input type="hidden" name="sent" value="true" />
+            </form>
           </div>
-          <form action="#" method="post" id="form1" class = "card card-sm transparent col-sm-3">
-            <input type="submit" class="btn btn-primary" name="action" value="Modifica"/>
-            <br/>
-            <input type="submit" class="btn btn-primary" name="action" value="Cancella"/>
-            <input type="hidden" name = "id" value="'.$_SESSION["id"].'">
-            <input type="hidden" name = "id" value="'.$row["nome_menu"].'">
-            <input type="hidden" name = "id" value="'.$row["id_ristorante"].'">
-            <input type="hidden" name = "id" value="'.$row["nome"].'">
-            <input type="hidden" name="sent" value="true" />
-          </form>
-        </div>
-        ';
+          ';
+        }
+      }else{
+        echo '<div class="card card-sm center-msg-box transparent col-sm-9 center">
+          <h3 class="text-center">il tuo carrello è vuoto :(</h3>
+        </div>';
       }
       ?>
   </div>
--- codice/php/ricerca.php
+++ codice/php/ricerca.php
@@ -30,6 +30,7 @@ function closeNav() {
     <link href="./../css/footer.css" rel="stylesheet">
     <link href="./../css/admin.css" rel="stylesheet">
     <link href="./../css/sidebar.css" rel="stylesheet">
+    <link href="./../css/ricerca.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   </head>
@@ -39,19 +40,50 @@ function closeNav() {
   var_dump($_SESSION["id_prenotazione"]); ?>
 
   <div id="mySidenav" class="sidenav">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <br/>
-    <a href="#">About</a>
-    <br/>
-    <a href="#">Services</a>
-    <br/>
-    <a href="#">Contact</a>
-    <br/>
-    <a href="#">Clients</a>
+    <div class ="margin">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <br/>
+      <h3>Categories</h3>
+      <form  action="#" method="post" id="Categories">
+        <div class="margin">
+
+          <?php
+          $servername = "localhost";
+          $username = "root";
+          $password = "";
+          $dbname = "cfu";
+
+          $conn = new mysqli($servername, $username, $password, $dbname);
+
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+          foreach($conn->query('SELECT * FROM categoria_ristoranti ORDER BY nome_categoria') as $row) {
+            echo'
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="'.$row['nome_categoria'].' name="check[]" id="'.$row['nome_categoria'].'">
+                <label class="form-check-label" for="'.$row['nome_categoria'].'">
+                  '.$row['nome_categoria'].'
+                </label>
+              </div>';
+          }
+
+          ?>
+        </div>
+        <br/>
+      </form>
+    </div>
+    <div class="row margin">
+      <input type="submit" class="btn btn-primary col-sm-6" name="action" value="Modifica" form="Categories"/>
+      <input type="submit" class="btn btn-primary col-sm-6" name="action" value="Modifica" form="Categories"/>
+    </div>
+
+    </div>
   </div>
 
+  <button type="button" name="button"  onclick="openNav()">Categories</button>
+
  <div class="main card card-sm center-msg-box main">
- <button type="button" name="button"  onclick="openNav()">Categories</button>
   <?php
   $servername = "localhost";
   $username = "root";
@@ -63,21 +95,35 @@ function closeNav() {
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  foreach($conn->query('SELECT * FROM ristorante ORDER BY nome_categoria') as $row) {
+  $filter = array();
+/*  foreach($conn->query('SELECT * FROM categoria_ristoranti ORDER BY nome_categoria') as $row) {
+    echo $row["nome_categoria"];*/
+    echo 'true';
+  if(isset($_POST['check[]'])){
+    $N = count($aCat);
+    for($i=0;$i < $N ; $i++){
+      $filter[$i] = 'nome_categoria = '.$aCat[$i];
+    }
+  }
+  echo implode(' ',$filter);
+  $query = 'SELECT * FROM ristorante WHERE 1=1'.implode(' AND ', $filter);
+  echo $query;
+  foreach($conn->query($query) as $row) {
     echo'
     <div class="row">
-      <div class="card card-sm center-msg-box col-sm-8">
-        <h3>'.$row["nome"].'</h3>
-        <h4>'.$row["nome_categoria"].'</h4>
-        <h6>'.$row["indirizzo"].'</h6>
+      <div class="card card-sm center-msg-box col-sm-9">
+        <h2>'.$row["nome"].'</h2>
+        <h3>'.$row["nome_categoria"].'</h3>
+        <h4>'.$row["indirizzo"].'</h4>
       </div>
-      <div class="card card-sm center-msg-box col-sm-4">
-        <h5> '.$row["rating"].'/10 utenti raccomandano </h5>
-        QUASI QUASI IL RATING LO TOGLIEREI
-      </div>
+      <form class=" col-sm-3 card card-sm center-msg-box" action="ristorante.php" method="post" name ="seleziona" id="seleziona">
+        <input type = "hidden" name="id_ristorante" value="'.$row["id"].'">
+        <button class="btn btn-sm btn-outline-info allign_center" id = "submit" type="submit" >Guarda Listino</button>
+      </form>
     </div>
       ';
     }
+    $conn->close();
     ?>
 
   </div>
