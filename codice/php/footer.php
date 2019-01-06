54c5250af5042b53a47c05b97dac674df4898185
Filippo Orazi <filippo.orazi@studio.unibo.it>
2019-01-06 19:39:48 +0100

    footer icone

--- codice/php/footer.php
+++ codice/php/footer.php
@@ -7,8 +7,15 @@
       <i class="fa fa-circle-thin fa-stack-2x"></i>
       <i class="fa fa-twitter fa-stack-1x"></i>
     </span>
-  </div>
-  <!-- Copyright -->
+    <span class="fa-stack fa-sm">
+      <i class="fa fa-facebook-square fa-2x"></i>
+    </span>
+    <span class="fa-stack fa-sm">
+      <i class="fa fa-instagram fa-2x"></i>
+    </span>
 
+  </div>
+
+  <!-- Copyright -->
 </footer>
 <!-- Footer -->
