74d3427d10e6708a60be9524daa766cb4034b06f
elizabetabudini <elizabeta.budini@studio.unibo.it>
2019-01-07 20:41:26 +0100

    evadi ordine aggiornato

--- codice/js/time.js
+++ codice/js/time.js
@@ -1,15 +1,21 @@
 $('#time').timepicker({
     'disableTimeRanges': [
+      ['7pm', '8pm'],
+      ['8pm', '9pm'],
+      ['10pm', '11pm'],
+      ['0am', '1am'],
       ['1am', '2am'],
+      ['2am', '3am'],
       ['3am', '4am'],
-      ['1am', '2am'],
-      ['1am', '2am'],
-      ['1am', '2am']
+      ['4am', '4am'],
+      ['5am', '6am'],
+      ['6am', '7am'],
+      ['7am', '8am'],
 
     ]
 });
 $('#time').timepicker({ 'step': 10 });
-$('#time').timepicker('option', { 'minTime': new Date(), 'maxTime': "10pm" });
+$('#time').timepicker('option', { 'minTime': new Date(), 'maxTime': "7pm" });
 
 $('#time').on('click', function (){
     $('#time').timepicker('show');
